c0cb10a15f04958b1dd6fa1ac1bffe9fb8f5ec5c
Riley Slayden <slaydenriley@gmail.com>
2020-01-22 17:15:04 +0000

    Automatically backed up by Learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -8,11 +8,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.find_by(:rating.maximum)
+    self.where("rating = ?", self.highest_rating).first
   end
 
   def self.least_popular_show
-    self.minimum(:rating)
+    self.where("rating = ?", self.lowest_rating).first
   end
 
   def self.ratings_sum
